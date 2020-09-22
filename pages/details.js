44b34a4c99c1880f1e4db92c0b973548c0794ccd
Patryk <patrickwww94@gmail.com>
2020-09-22 19:50:53 +0200

    Add details page

---
+++ pages/details.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CampaignDetails extends Component {
+  render() {
+    return <h3>Campaign Show</h3>;
+  }
+}
+
+export default CampaignDetails;
--- routes.js
+++ routes.js
@@ -1,3 +1,6 @@
 const routes = require("next-routes")();
 
+routes.add('/new', '/new')
+routes.add('/:address', '/details');
+
 module.exports = routes;
