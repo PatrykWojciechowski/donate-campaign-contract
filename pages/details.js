75f6253e195550452f167a6e7601cebff6e5819d
Patryk <patrickwww94@gmail.com>
2020-09-25 11:06:30 +0200

    Update details page

--- pages/details.js
+++ pages/details.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class CampaignDetails extends Component {
   render() {
-    return <h3>Campaign Show</h3>;
+    return (
+        <Layout>
+          <h3>Campaign Show</h3>
+        </Layout>
+  );
   }
 }
 
